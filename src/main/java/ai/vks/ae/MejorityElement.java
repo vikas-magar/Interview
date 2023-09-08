b58aebd9ccc1b734199f1c29c73ad89835785246
Vikas Ksr <vikas.b.magar@outlook.com>
2023-09-08 07:31:36 +0530

    AE | Majority Element

--- src/main/java/ai/vks/ae/MejorityElement.java
+++ src/main/java/ai/vks/ae/MejorityElement.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class MejorityElement {
     /*Time: O(n) | Space: O(n)*/
-    public int majorityElement(int[] array) {
+    public int majorityElement1(int[] array) {
         HashMap<Integer, Integer> store = new HashMap<>();
         if (array.length < 1) return -1;
         else if (array.length == 1) {
@@ -25,5 +25,27 @@ public class MejorityElement {
             }
         }
         return (majorityK >= array.length / 2) ? majorityE : -1;
+    }
+    public int majorityElement(int[] array){
+        var majorityE = -1;
+        var majorityC = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0){majorityE=array[i]; majorityC = 1;}
+            if (majorityC == 0){majorityE=array[i+1]; majorityC = 1;}
+            else {
+                if (array[i] == majorityE) majorityC += 1;
+                else {
+                     majorityC -= 1;
+                }
+            }
+        }
+        System.out.println(majorityE);
+        return majorityE;
+    }
+
+    public static void main(String[] args) {
+//        var input = new int[] {1, 2, 3, 2, 2, 1, 2};
+        var input = new int[] {1, 1, 1, 1, 2, 2, 2, 2, 2};
+        new MejorityElement().majorityElement(input);
     }
 }
